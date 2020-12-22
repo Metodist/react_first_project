10f4ebe88bdc88b3e2b14054bd5fba90a7cfb8b1
Iona M <51836963+Metodist@users.noreply.github.com>
2020-12-22 07:40:56 +0300

    20 урок прошел - Navlink

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import style from './Navigation.module.css';
+import {NavLink} from "react-router-dom";
 
-const Nav = () =>{
+const Nav = () => {
     return <nav className={style.nav}>
-                <div className={`${style.item} ${style.active}`}>
-                    <a href='profile'>Profiles</a>
-                </div>
-                <div className={style.item}>
-                    <a href='message'>Messages</a>
-                </div>
-                <div className={style.item}>
-                    <a href = 'news'>News</a>
-                </div>
-                <div className={style.item}>
-                    <a href = 'music'>Music</a>
-                </div>
-                <div className={style.item}>
-                    <a href = 'setting'>Settings</a>
-                </div>
-            </nav>
+        <div className={`${style.item} ${style.active}`}>
+            <NavLink to='profile' activeClassName={style.active}>Profiles</NavLink>
+        </div>
+        <div className={style.item}>
+            <NavLink to='message' activeClassName={style.active}>Messages</NavLink>
+        </div>
+        <div className={style.item}>
+            <NavLink to='news' activeClassName={style.active}>News</NavLink>
+        </div>
+        <div className={style.item}>
+            <NavLink to='music' activeClassName={style.active}>Music</NavLink>
+        </div>
+        <div className={style.item}>
+            <NavLink to='setting' activeClassName={style.active}>Settings</NavLink>
+        </div>
+    </nav>
 }
 
 export default Nav;
