4fbb7a33e5280404937e6ec1000d0bffc134f490
Iona M <51836963+Metodist@users.noreply.github.com>
2020-12-22 06:01:02 +0300

    18 урок прошел

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Header from "./components/header/Header";
 import Nav from "./components/navigation/Navigation";
 import Profile from "./components/profile/Profile";
 import React from "react";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
             <Header/>
             <Nav/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
+
 
         </div>
     );
