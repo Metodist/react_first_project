94a70f9b503ea6c0b4fa6370fe5dda8c56b73bd3
Iona M <51836963+Metodist@users.noreply.github.com>
2020-12-24 11:19:23 +0300

    21 урок прошел - сверстали диалоги

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import Style from './Dialogs.module.css';
 
-const Dialogs = () =>{
+
+
+const Dialogs = (props) =>{
     return(
-        <div>
-            Dialogs
+        <div className={Style.messages}>
+            <div className={Style.dialogs}>
+                <div className={Style.dialogsItem + ' ' + Style.active}>
+                    name 1
+                </div>
+                <div className={Style.dialogsItem}>
+                    name 2
+                </div>
+                <div className={Style.dialogsItem}>
+                    name 3
+                </div>
+            </div>
+            <div className={Style.dialog}>
+                <div className={Style.dialogItem}>
+                    text 1
+                </div>
+                <div className={Style.dialogItem}>
+                    text 2
+                </div>
+                <div className={Style.dialogItem}>
+                    text 3
+                </div>
+            </div>
         </div>
     )
 }
